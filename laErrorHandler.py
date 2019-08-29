65597b215eef49fbb005b9663e5a6ae11b62b98a
Matheus Kirstus <matheus.kirs@gmail.com>
2019-08-28 21:37:04 -0300

    ajustes para mais casos de teste

--- laErrorHandler.py
+++ laErrorHandler.py
@@ -11,40 +11,18 @@ class laErrorHandler(ErrorListener):
 		message = msg
 		symbols = []
 
-		'''print(o)
-		print(line)
-		print(msg)
-		print(value)'''
-		#self.output.write("erorhandler\n")
-
-		if(message[0:10] == "mismatched"):
+		if(message[0:20] == "mismatched input '{'"):
+        	#print("Linha " + str(line+1) + ": comentario nao fechado")
 			self.output.write("Linha " + str(line+1) + ": comentario nao fechado\n")
-			#print("Linha " + str(line+1) + ": comentario nao fechado")
-		elif(message[0:9] == "no viable"):
+		elif(message[0:9] == "no viable" and len(message) == 34):
+            #print("Linha " + str(line) + ": " + value + " - simbolo nao identificado")
 			self.output.write("Linha " + str(line) + ": " + value + " - simbolo nao identificado\n")
-			#print("Linha " + str(line) + ": " + value + " - simbolo nao identificado")
 		else:
-			self.output.write("Linha " + str(line) + ": erro sintatico proximo a " + value + "\n")
 			#print("Linha " + str(line) + ": erro sintatico proximo a " + value)
+			if(value == "<EOF>"):
+				value = "EOF"
+			self.output.write("Linha " + str(line) + ": erro sintatico proximo a " + value + "\n")
+
 		self.output.write("Fim da compilacao\n")
-		#print("Fim da compilacao")
+        #print("Fim da compilacao")
 		exit()
-		'''print("Line " + str(line) + ": ", end="")
-		if(value == "<EOF>" and message[0:10] == "mismatched"):
-			print("Job parameter has to be either 'class' or 'regression'\n")
-		elif(value == "[" and message[0:10] == "mismatched"):
-			print("features/classes range or training data size missing\n")
-		elif(message[0:10] == "extraneous"):
-			print("Erroneous value '" + value + "' found")
-		elif(message[-5:] == "IDENT" and message[0:10] == "mismatched" and value.isdigit()):
-			print("Dataset name is missing")
-		elif(message[0:7] == "missing"):
-			split = message.split("'")[1]
-			print("Expected '" + split + "' next to '" + value + "'\n")
-		elif(message[-3:] == "NUM" and message[0:10] == "mismatched"):
-			print("Number is expected instead of '" + value + "'\n")
-		elif(message[-5:] == "IDENT" and message[0:10] == "mismatched"):
-			print("Name expected next to '" + value + "'\n")
-		else:
-			print("Syntax Error\n")
-		exit()'''
